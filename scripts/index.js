23ec7db80afa356e7994f02f59150f782b9d7751
imAlexDuh <4uva40k99@gmail.com>
2023-04-26 18:47:08 +0300

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,32 @@
 let profile = document.querySelector('.profile')
-let editbutton = profile.querySelector('.profile__edit-button')
+let editButton = profile.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
-let closebutton = popup.querySelector('.popup__close-button')
-let authorinfo = profile.querySelector('.profile__info')
-let author = authorinfo.querySelector('.profile__author')
-let profilename = author.querySelector('.profile__author-name') 
-let popupname = popup.querySelector('.popup__text_name')
+let closeButton = popup.querySelector('.popup__close-button')
+let authorInfo = profile.querySelector('.profile__info')
+let author = authorInfo.querySelector('.profile__author')
+let profileName = author.querySelector('.profile__author-name')
+let popupName = popup.querySelector('.popup__text-name')
 let info = author.querySelector('.profile__author-info')
-let popupinfo = popup.querySelector('.popup__text_info')
-editbutton.addEventListener('click', popupOpen)
-closebutton.addEventListener('click', popupClose)
+let popupInfo = popup.querySelector('.popup__text-info')
+let formElement = popup.querySelector('.popup__fields')
 
 function popupOpen() {
-    popupname.value = profilename.textContent;
-    popupinfo.value = info.textContent;
-    popup.classList.add('popup__opened'); 
+    popupName.value = profileName.textContent;
+    popupInfo.value = info.textContent;
+    popup.classList.add('popup_opened');
 }
 
 function popupClose() {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
 
-// Находим форму в DOM
-let formElement = popup.querySelector('.popup__fields')// Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
     evt.preventDefault();
-    profilename.textContent = popupname.value;
-    info.textContent = popupinfo.value;
-    popupClose(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    profileName.textContent = popupName.value;
+    info.textContent = popupInfo.value;
+    popupClose();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+editButton.addEventListener('click', popupOpen)
+closeButton.addEventListener('click', popupClose)
 formElement.addEventListener('submit', handleFormSubmit); 
